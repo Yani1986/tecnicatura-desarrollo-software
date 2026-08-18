5835295056ddaf10ea5261a4dcb248964c8708ee
Yanina Rodriguez <yani_rodriguez@outlook.com>
2026-08-17 23:03:25 -0300

    Día 4: uso de condicionales/Comenzamos con Java

---
+++ Técnicas de programación/Java/HolaMundo.java
@@ -0,0 +1,18 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.print("Hola mundo ");
+        System.out.println("Estoy usando Java ");
+        int edad = 15;
+        double altura = 1.65;
+        boolean estudiaJava = true;
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(estudiaJava);
+        if (edad >= 18) {System.out.println("Es mayor de edad");}
+        else {System.out.println("Es menor de edad");
+                }
+
+    }
+
+}
